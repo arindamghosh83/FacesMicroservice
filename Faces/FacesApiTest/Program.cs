3b0dd906c10f85ec99aa8c866d994ec7c087bbc1
arindam <aghosh1@cmh.edu>
2020-09-26 10:17:13 -0500

    initial commit
    initial commit

---
+++ Faces/FacesApiTest/Program.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace FacesApiTest
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var imagePath = @"image.jpg";
+            var urlAddress = "http://localhost:6001/api/faces";
+            ImageUtility imgUtil = new ImageUtility();
+            var bytes = imgUtil.ConvertToBytes(imagePath);
+            List<byte[]> faceList = null;
+            var byteContent = new ByteArrayContent(bytes);
+            byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+            using( var httpClient = new HttpClient())
+            {
+                using(var response = await httpClient.PostAsync(urlAddress, byteContent))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    faceList = JsonConvert.DeserializeObject<List<byte[]>>(apiResponse);
+                }
+            }
+            if (faceList.Count > 0)
+            {
+                for(int i=0;i <faceList.Count; i++)
+                {
+                    imgUtil.FromBytesToImage(faceList[i], "face" + i);
+                }
+            }
+        }
+    }
+}
