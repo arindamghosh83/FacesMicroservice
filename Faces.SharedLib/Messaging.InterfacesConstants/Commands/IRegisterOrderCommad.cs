d0a9c10c7a55f2e624490f2acf48c35e6c6573b0
arindam <aghosh1@cmh.edu>
2020-09-27 12:41:08 -0500

    .gitignore fix2

---
+++ Faces.SharedLib/Messaging.InterfacesConstants/Commands/IRegisterOrderCommad.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Messaging.InterfacesConstants.Commands
+{
+    public interface IRegisterOrderCommad
+    {
+        public Guid OrderId { get; set; }
+        public string ImageUrl { get; set; }
+        public string UserEmail { get; set; }
+        public byte[] ImageData { get; set; }
+    }
+}
